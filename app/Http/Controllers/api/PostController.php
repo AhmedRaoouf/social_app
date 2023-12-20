2a4da8364ac18ac7fa10c0ca21d4165dd315d0b7
RaOouf <ahmedraoouf123@gmail.com>
2023-12-20 11:55:41 +0200

    edit posts2

--- app/Http/Controllers/api/PostController.php
+++ app/Http/Controllers/api/PostController.php
@@ -35,7 +35,7 @@ class PostController extends Controller
                 'content' => $request->content,
                 'image' => $userImage,
             ]);
-            
+
             return helper::responseData(new PostResource($post), 'Post created successfully');
         } else {
             return helper::responseError('Missing data in request');
@@ -69,15 +69,19 @@ class PostController extends Controller
         $token = $request->header('Authorization');
         $user = User::where('token', $token)->first();
         $post = Post::where('id', $postId)->first();
-        if ($request->content) {
-            $comment = new Comment(['user_id' => $user->id, 'post_id' => $postId,'content' =>$request->content]);
-            $post->post_comments()->save($comment);
-            $message = 'Comment added successfully';
-            $post->increment('total_comments');
-            $totalComments = $post->total_comments;
-            return helper::responseData(['total_comments' => $totalComments,'comment' => $request->content], $message);
+        if ($post) {
+            if ($request->content) {
+                $comment = new Comment(['user_id' => $user->id, 'post_id' => $postId,'content' =>$request->content]);
+                $post->post_comments()->save($comment);
+                $message = 'Comment added successfully';
+                $post->increment('total_comments');
+                $totalComments = $post->total_comments;
+                return helper::responseData(['total_comments' => $totalComments,'comment' => $request->content], $message);
+            }else{
+                return helper::responseError('Should add comment');
+            }
         }else{
-            return helper::responseError('Should add comment');
+            return helper::responseError('Invalid post');
         }
     }
 }
